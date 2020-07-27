e6ee74794cca976866e3500e02196aae5a1074cf
Amarnath Sakthivel <63423850+Amarnath-Sakthivel@users.noreply.github.com>
2020-07-27 20:01:15 +0530

    Add files via upload

---
+++ JumbledWords.java
@@ -0,0 +1,32 @@
+package com.wipro.javapractice;
+
+public class JumbledWords {
+	public static void JumbledWords(String input1, int input2) {
+
+		String out="";
+		for (String split : input1.split(" ")) {
+			String odd ="", even ="";
+			for (int i = 0; i < split.length(); i++) {
+				if (i % 2 == 0)
+					odd += split.charAt(i);
+				else {
+					even += split.charAt(i);
+				}
+			}
+			if (input2 == 2)
+				out+= odd.concat(even)+" ";
+			else if (input2 == 1)
+				out+= odd.concat(new StringBuilder(even).reverse().toString())+" ";
+
+		}
+		System.out.println(out.trim());
+	}
+
+	
+
+	public static void main(String[] args) {
+		JumbledWords("Wipro Technologies Bangalore", 1);
+		JumbledWords("Wipro Technologies Bangalore ", 2);
+	}
+
+}
